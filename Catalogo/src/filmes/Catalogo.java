12dd7401b018536d45004231810673aca4a9dc1a
AlexReisC <alex.reis@aluno.ufca.edu.br>
2023-03-27 18:14:29 -0300

    Implementando os metodos das classes

--- Catalogo/src/filmes/Catalogo.java
+++ Catalogo/src/filmes/Catalogo.java
@@ -5,22 +5,40 @@ import java.util.List;
 
 public class Catalogo {
     private List<Filme> filmes;
-    
-    public Catalogo(List<Filme> f){
-        this.filmes = new ArrayList<>();
+
+    public Catalogo(){
+        filmes = new ArrayList<>();
     }
 
     public void adicionarFilme(Filme f){
         filmes.add(f);
-        System.out.println("Filme adicionado ao catalogo!");
+        System.out.println("Filme " + f.getTitulo() + " adicionado ao catalogo.");
     }
 
     public void listarFilmes(){
-        
+        System.out.println("\n-----------------");
+        System.out.println("Filmes no catalogo:");
+        for (int i = 0; i < filmes.size() ; i++) {
+            System.out.println(filmes.get(i).getTitulo());
+        }
+        System.out.println("-----------------\n");
     }
 
-    public Filme buscar(Filme f){
-           
+    public void excluirFilme(Filme f){
+        if(filmes.isEmpty()){
+            System.out.println("Catalogo vazio!");
+        } else{
+            for (int i = 0; i < filmes.size(); i++) {
+                if(filmes.get(i) == f){
+                    filmes.remove(f);
+                }
+            }
+            System.out.println("O filme foi removido.");
+        }
     }
+
+    /* public Filme buscar(Filme f){
+           
+    } */
 
 }
