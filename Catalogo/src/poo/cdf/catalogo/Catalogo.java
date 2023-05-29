3c7028ad927c674aff6eda762b87da4c9fbcda37
alexreis <alex.reis@aluno.ufca.edu.br>
2023-05-29 09:58:20 -0300

    lista de favoritos e metodos para add

--- Catalogo/src/poo/cdf/catalogo/Catalogo.java
+++ Catalogo/src/poo/cdf/catalogo/Catalogo.java
@@ -10,7 +10,7 @@ public class Catalogo{
     // Lista das midias
     private List<Midia> midias;
     // Lista de usuarios
-    private List<Usuaro> usuarios;
+    private List<Usuario> usuarios;
     // lista de generos favoritos
 
     public Catalogo(){
@@ -121,17 +121,4 @@ public class Catalogo{
 
 
     // Lista de usuarios: login e senha
-
-    /* FUNCIONALIDADES
-    Permitir que os usuários possam pesquisar por filmes e séries utilizando palavras-chave, gênero, ano de lançamento, classificação etária, entre outros filtros.
-
-    Permitir que os administradores do sistema possam cadastrar novos títulos, incluindo informações como nome, sinopse, elenco, diretor, gênero, classificação, entre outras informações relevantes.
-
-    Permitir que os usuários possam marcar títulos como favoritos ou adicionar à sua lista de interesse, facilitando a navegação e a escolha do que assistir.
-
-    Permitir que os usuários possam avaliar e escrever críticas sobre os títulos, bem como visualizar as avaliações e críticas de outros usuários.
-
-    Permitir que os usuários possam receber notificações sobre novos lançamentos e títulos adicionados ao catálogo de gêneros marcados como favorito.
-
- */
 }
--- Catalogo/src/poo/cdf/catalogo/Usuario.java
+++ Catalogo/src/poo/cdf/catalogo/Usuario.java
@@ -1,17 +1,35 @@
 package poo.cdf.catalogo;
 
 import java.util.ArrayList;
+import poo.cdf.midia.*;
 
 public class Usuario {
     private String nome;
     private String login;
     private String senha;
-    private ArrayList<Midias> favoritos = new ArrayList<>();
+    private ArrayList<Midia> favoritos = new ArrayList<>();
 
     public Usuario(String nome, String login, String senha){
         this.nome = nome;
         this.login = login;
         this.senha = senha;
+    }
+
+    // Marcar midias como favoritas
+    public void favoritar(Midia m){
+        favoritos.add(m);
+    }
+
+    public void listarFavoritos(){
+        if(favoritos.isEmpty()){
+            System.out.println("Sem favoritos adicionados");
+        } else{
+            System.out.println("Seus favoritos");
+            for (Midia fav : favoritos) {
+                System.out.println("Titulo: " + fav.getTitulo() + " - " + fav.getGenero() + " (" + fav.getAnoDeEstreia() + ")");
+            }
+            System.out.println("------------------\n");
+        }
     }
 
     /* Permitir que os usuários possam pesquisar por filmes e séries utilizando palavras-chave, gênero, ano de lançamento, classificação etária, entre outros filtros.
@@ -21,4 +39,5 @@ public class Usuario {
     Permitir que os usuários possam avaliar e escrever críticas sobre os títulos, bem como visualizar as avaliações e críticas de outros usuários.
 
     Permitir que os usuários possam receber notificações sobre novos lançamentos e títulos adicionados ao catálogo de gêneros marcados como favorito. */
+    
 }
