102f2b3eb3834f8413c6de526ead97bb8d21bf3b
AlexReisC <alex.reis@aluno.ufca.edu.br>
2023-06-11 14:59:27 -0300

    :test_tube: test: Testando metodos

--- Catalogo/src/br/edu/poo/Main.java
+++ Catalogo/src/br/edu/poo/Main.java
@@ -12,21 +12,21 @@ public class Main {
         Serie s1 = new Serie();
 
         Administrador adm = new Administrador("Alex", "Alek2", "12345");
-        adm.adicionarMidia(filme2, c.midias);
+        //adm.adicionarMidia(filme2, c.midias);
         /* adm.adicionarMidia(filme1, c.midias);
         adm.adicionarMidia(s1, c.midias); */
         //c.listarMidias();
         //adm.removerrMidia(filme2, c.midias);
 
         Usuario u1 = new Usuario("Alex");
-        u1.cadastrar(c.usuarios, "Alek", "1234");
+        //u1.cadastrar(c.usuarios, "Alek", "1234");
         //u1.login(c, "Alek", "1234");
         //c.listarUsuarios();
         //u1.pesquisarPorAnoDeEstreia(2008, c.midias);
         //u1.pesquisarPalavraChave("Homem", c.midias);
         //u1.pesquisarPorClassificao(12, c.midias);
         //u1.pesquisarPorGenero("Super heroi", c.midias);
-        //u1.favoritar("Homem de Ferro", c.midias);
+        u1.favoritar("Homem de Ferro", c.midias);
         //u1.listarFavoritos();
         //u1.avaliar(filme2, c.midias);
     }
